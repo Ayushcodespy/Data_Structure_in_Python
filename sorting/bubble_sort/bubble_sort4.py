17e811e7c064284fa2caa1eec45d00a844a65080
Ayush <ayush.jnv54@gmail.com>
2024-07-21 22:18:19 +0530

    Added more python programs to perform bubble sort

---
+++ sorting/bubble_sort/bubble_sort4.py
@@ -0,0 +1,15 @@
+lst = [12,65,34,90,65,76,85,39,29,81]
+length = len(lst)
+
+def bubblesort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if(arr[j]>arr[j+1]):
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+        
+
+print(lst)
+bubblesort(lst)
+print(lst)
