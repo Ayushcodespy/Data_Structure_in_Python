a99850a3039ebd8dec2846ac91d9ea453ebcbce4
Ayush <ayush.jnv54@gmail.com>
2024-07-30 00:00:45 +0530

    Creating linked lists using python

--- linked_list/singly_linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list/singly_linked_list.py
@@ -13,3 +13,26 @@ class SLL:
     def insert_at_start(self, data):
         new = Node(data, self.head)
         self.head = new
+
+    def insert_at_last(self, data):
+        new = Node(data)
+        if not self.is_empty():
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new
+
+        else:
+            self.head = new
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data, end="  ")
+            temp = temp.next
+
+lst = SLL()
+lst.insert_at_start(24)
+lst.insert_at_start(27)
+lst.insert_at_last(73)
+lst.print_list()
