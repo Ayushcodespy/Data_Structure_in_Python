13c6e44224758d5426ee7465e892c624d8e3d53b
Ayush <ayush.jnv54@gmail.com>
2024-07-31 00:25:37 +0530

    All insertion operation in linkd lists in python

--- linked_list/singly_linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list/singly_linked_list.py
@@ -1,15 +1,16 @@
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, _next=None):
         self.data = data
-        self.next = next
+        self.next = _next
+
 
 class SLL:
-    def __init__(self, head = None):
+    def __init__(self, head=None):
         self.head = head
 
     def is_empty(self):
-        return self.head == None
-    
+        return self.head is None
+
     def insert_at_start(self, data):
         new = Node(data, self.head)
         self.head = new
@@ -25,14 +26,62 @@ class SLL:
         else:
             self.head = new
 
+    def insert_after(self, index, data):
+        if self.is_empty():
+            print("List is empty")
+            return
+
+        temp = self.head
+        for i in range(index):
+            if temp is None:
+                print("Index is out of bounds.")
+            temp = temp.next
+
+        if temp is None:
+            print("Index is out of bounds.")
+            return
+
+        new = Node(data, temp.next)
+        temp.next = new
+
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.data, end="  ")
             temp = temp.next
+        print("\n")
+
+
+def runcode(arr):
+    print('''\n Select operations in Linked List: 
+        1. Insert at start
+        2. Insert at last
+        3. Insert at specific place
+        4. Show list''')
+    choice = int(input("Select Operation : "))
+    if choice == 1:
+        data = int(input("Enter data to insert : "))
+        arr.insert_at_start(data)
+
+    elif choice == 2:
+        data = int(input("Enter data to insert : "))
+        arr.insert_at_last(data)
+
+    elif choice == 3:
+        data = int(input("Enter data to insert : "))
+        index = int(input("In which place you want to insert : "))
+        index = index - 1
+        arr.insert_after(index, data)
+
+    elif choice == 4:
+        arr.print_list()
+
+    else:
+        print("Please select a correct option")
+
 
 lst = SLL()
-lst.insert_at_start(24)
-lst.insert_at_start(27)
-lst.insert_at_last(73)
-lst.print_list()
+loop = 1
+while loop == 1:
+    runcode(lst)
+    loop = int(input("Press 1 to continue : "))
